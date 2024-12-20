2c5f0a077ae17333379afa0f9811c160f1702cc6
Evgeniy <borovoyevgen@gmail.com>
2024-12-21 02:39:50 +0400

    Initial Commit

---
+++ RemindersSwiftData/RemindersSwiftDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  RemindersSwiftDataApp.swift
+//  RemindersSwiftData
+//
+//  Created by Evgeniy Borovoy on 12/21/24.
+//
+
+import SwiftUI
+
+@main
+struct RemindersSwiftDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
